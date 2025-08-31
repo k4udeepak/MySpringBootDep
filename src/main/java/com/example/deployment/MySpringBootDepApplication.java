b556ea537000dad4906d17498ad9ec85871f32d2
k4udeepak <p4udeepak@gmail.com>
2025-08-31 11:13:49 +0530

    Initial commit

---
+++ src/main/java/com/example/deployment/MySpringBootDepApplication.java
@@ -0,0 +1,13 @@
+package com.example.deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootDepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootDepApplication.class, args);
+	}
+
+}
